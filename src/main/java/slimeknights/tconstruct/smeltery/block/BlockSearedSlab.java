b96aacd75a73a6f2af9859383309e908d306b3cf
Sokratis Fotkatzikis <sokratis12GR@users.noreply.github.com>
2017-11-16 23:09:51 +0200

    Used more "correct" methods for the Tile Entity Holder

--- src/main/java/slimeknights/tconstruct/smeltery/block/BlockSearedSlab.java
+++ src/main/java/slimeknights/tconstruct/smeltery/block/BlockSearedSlab.java
@@ -22,7 +22,7 @@ import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.smeltery.TinkerSmeltery;
 import slimeknights.tconstruct.smeltery.tileentity.TileSmelteryComponent;
 
-public class BlockSearedSlab extends EnumBlockSlab<BlockSearedSlab.SearedType> implements ITileEntityProvider {
+public class BlockSearedSlab extends EnumBlockSlab<BlockSearedSlab.SearedType> {
 
   public final static PropertyEnum<SearedType> TYPE = PropertyEnum.create("type", SearedType.class);
 
@@ -32,7 +32,6 @@ public class BlockSearedSlab extends EnumBlockSlab<BlockSearedSlab.SearedType> i
     this.setHardness(3F);
     this.setResistance(20F);
     this.setSoundType(SoundType.METAL);
-    this.isBlockContainer = true; // has TE
   }
 
   @Override
@@ -44,11 +43,16 @@ public class BlockSearedSlab extends EnumBlockSlab<BlockSearedSlab.SearedType> i
   }
   
   /* Multiblock stuff */
-
+  
   @Nonnull
   @Override
-  public TileEntity createNewTileEntity(@Nonnull World worldIn, int meta) {
+  public TileEntity createTileEntity(@Nonnull World world,@Nonnull  IBlockState state) {
     return new TileSmelteryComponent();
+  }
+
+  @Override
+  public boolean hasTileEntity(IBlockState state) {
+    return true;
   }
 
   @Override
